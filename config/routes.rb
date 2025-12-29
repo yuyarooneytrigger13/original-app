663af7ea9fabddf239337bfe066c2c031bca81d8
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2025-12-29 12:20:37 +0900

    ログアウト、top,indexアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#index"
+  root to: "homes#top"
   resources :homes, only: [:index]
+  resources :plans, only: [:index]
   
 end
